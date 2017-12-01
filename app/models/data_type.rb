a5a711956403dc9a562e763fb088a437b6552a3b
Raghu Betina <raghu@firstdraft.com>
2017-12-01 15:31:30 +0000

    Direct association between data_types and prescriptions

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,6 +1,10 @@
 class DataType < ApplicationRecord
   # Direct associations
 
+  has_many   :prescriptions,
+             :foreign_key => "datatype_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
