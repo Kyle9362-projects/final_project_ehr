c3aa40271dd0708c9dc5648a1649a38dba461773
Raghu Betina <raghu@firstdraft.com>
2017-12-01 15:32:15 +0000

    Whitelist columns in Record for ActiveAdmin

--- app/admin/record.rb
+++ app/admin/record.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Record do
 
+ permit_params :prescription_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
